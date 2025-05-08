91de65284a37bb1829e3066fcaaaf136de23294e
TheFiles <info@thefiles.io>
2025-05-08 19:21:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Index from "./pages/Index";
 import Imprint from "./pages/Imprint";
@@ -34,6 +34,5 @@ const App = () => {
     </QueryClientProvider>
   );
 };
-
 
 export default App;
